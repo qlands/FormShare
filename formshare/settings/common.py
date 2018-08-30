f5e70defde6ce61756e9ed104150dc287bb8faaf
Carlos Quiros <cquiros@qlands.com>
2018-08-29 23:02:55 -0600

    Change root
    Remove /formshare/ as root so the testing server does not crash.

--- formshare/settings/common.py
+++ formshare/settings/common.py
@@ -78,7 +78,7 @@ USE_L10N = True
 
 # This is the application root.
 # Example:" /formshare/ for hosting Formshare in like http://example.com/formshare
-APP_ROOT = '/formshare/'
+APP_ROOT = '/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.

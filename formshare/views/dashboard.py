6c00aeca15f26d5fbeb6228cb46d51c3a6be8b4c
Carlos Quiros <cquiros@qlands.com>
2018-12-12 22:00:07 -0600

    Fix no projects bug

--- formshare/views/dashboard.py
+++ formshare/views/dashboard.py
@@ -24,36 +24,42 @@ class UserDashBoardView(DashboardView):
                         project_data["user_collaborates"] = False
             if not project_found:
                 raise HTTPNotFound
-        else:
-            raise HTTPNotFound
-
-        assistants, more_assistants = get_project_assistants(self.request, self.activeProject['project_id'], 8)
-        if self.user is not None:
-            collaborators, more_collaborators = get_project_collaborators(self.request,
-                                                                          self.activeProject['project_id'],
-                                                                          self.user.login, 4)
-        else:
-            collaborators, more_collaborators = get_project_collaborators(self.request,
-                                                                          self.activeProject['project_id'], None, 4)
-        user_details = get_user_details(self.request, user_id)
-        forms = get_project_forms(self.request, user_id, self.activeProject['project_id'])
-        active_forms = 0
-        inactive_forms = 0
-        for form in forms:
-            if form['form_accsub'] == 1:
-                active_forms = active_forms + 1
+        
+            assistants, more_assistants = get_project_assistants(self.request, self.activeProject['project_id'], 8)
+            if self.user is not None:
+                collaborators, more_collaborators = get_project_collaborators(self.request,
+                                                                              self.activeProject['project_id'],
+                                                                              self.user.login, 4)
             else:
-                inactive_forms = inactive_forms + 1
+                collaborators, more_collaborators = get_project_collaborators(self.request,
+                                                                              self.activeProject['project_id'], None, 4)
+            user_details = get_user_details(self.request, user_id)
+            forms = get_project_forms(self.request, user_id, self.activeProject['project_id'])
+            active_forms = 0
+            inactive_forms = 0
+            for form in forms:
+                if form['form_accsub'] == 1:
+                    active_forms = active_forms + 1
+                else:
+                    inactive_forms = inactive_forms + 1
 
-        submissions, last, by, in_form = get_dataset_stats_for_project(self.request, user_id,
-                                                                       self.activeProject['project_code'])
+            submissions, last, by, in_form = get_dataset_stats_for_project(self.request, user_id,
+                                                                           self.activeProject['project_code'])
 
-        bydetails = get_by_details(self.request, user_id, self.activeProject['project_id'], by)
-        return {'projectData': project_data, 'userid': user_id, 'collaborators': collaborators,
-                'moreCollaborators': more_collaborators, 'assistants': assistants, 'moreAssistants': more_assistants,
-                'groups': get_project_groups(self.request, self.activeProject['project_id']),
-                'files': get_project_files(self.request, self.activeProject['project_id']), 'userDetails': user_details,
-                'forms': forms, 'activeforms': active_forms, 'inactiveforms': inactive_forms,
-                'submissions': submissions, 'last': last, 'by': by, 'bydetails': bydetails, 'infom': in_form,
-                'withgps': get_number_of_datasets_with_gps_in_project(self.request, user_id,
-                                                                      self.activeProject['project_code'])}
+            bydetails = get_by_details(self.request, user_id, self.activeProject['project_id'], by)
+            return {'projectData': project_data, 'userid': user_id, 'collaborators': collaborators,
+                    'moreCollaborators': more_collaborators, 'assistants': assistants, 'moreAssistants': more_assistants,
+                    'groups': get_project_groups(self.request, self.activeProject['project_id']),
+                    'files': get_project_files(self.request, self.activeProject['project_id']), 'userDetails': user_details,
+                    'forms': forms, 'activeforms': active_forms, 'inactiveforms': inactive_forms,
+                    'submissions': submissions, 'last': last, 'by': by, 'bydetails': bydetails, 'infom': in_form,
+                    'withgps': get_number_of_datasets_with_gps_in_project(self.request, user_id,
+                                                                          self.activeProject['project_code'])}
+        else:
+            return {'projectData': None, 'userid': user_id, 'collaborators': [],
+                    'moreCollaborators': 0, 'assistants': [], 'moreAssistants': 0,
+                    'groups': [],
+                    'files': [], 'userDetails': None,
+                    'forms': [], 'activeforms': 0, 'inactiveforms': 0,
+                    'submissions': 0, 'last': None, 'by': None, 'bydetails': None, 'infom': 0,
+                    'withgps': 0}

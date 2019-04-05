354d73faa64089cf6a58349a7020065d629d2b7a
Carlos Quiros <cquiros@qlands.com>
2019-04-04 21:39:27 -0600

    Download media product

---
+++ formshare/products/export/media/__init__.py
@@ -0,0 +1,1 @@
+from .media import generate_media_zip_file
---
+++ formshare/products/export/media/media.py
@@ -0,0 +1,21 @@
+from formshare.products import register_product_instance
+from .celery_task import build_media_zip
+import uuid
+import os
+
+
+def generate_media_zip_file(request, user, project, form, odk_dir, form_directory, form_schema, primary_key):
+    settings = {}
+    for key, value in request.registry.settings.items():
+        if isinstance(value, str):
+            settings[key] = value
+
+    uid = str(uuid.uuid4())
+    paths = ['products', uid + ".zip"]
+    repo_dir = request.registry.settings['repository.path']
+    media_file = os.path.join(repo_dir, *paths)
+
+    task = build_media_zip.apply_async((settings, odk_dir, form_directory, form_schema, media_file, primary_key),
+                                       countdown=2)
+    register_product_instance(request, user, project, form, 'media_export', task.id, media_file,
+                              'application/zip', False)

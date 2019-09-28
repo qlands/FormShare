145bc6519094c9cf37f2d5aa9a6c83d2dabbb2a1
Carlos Quiros <cquiros@qlands.com>
2019-09-28 12:32:25 -0600

    New create_config tool

--- create_config.py
+++ create_config.py
@@ -1,24 +1,54 @@
-import sys
 import os
 from jinja2 import Environment, FileSystemLoader
+import argparse
+import random
+import string
+import uuid
 
 
-def usage(argv):
-    cmd = os.path.basename(argv[0])
-    print(
-        "usage: %s <path_to_ini_file>\n" "(example: %s ./development.ini )" % (cmd, cmd)
+def random_password(size):
+    """Generate a random password """
+    random_source = string.ascii_letters + string.digits + string.punctuation
+    password = random.choice(string.ascii_lowercase)
+    password += random.choice(string.ascii_uppercase)
+    password += random.choice(string.digits)
+    password += random.choice(string.punctuation)
+    for i in range(size):
+        password += random.choice(random_source)
+    password_list = list(password)
+    random.SystemRandom().shuffle(password_list)
+    password = "".join(password_list)
+    return password
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ini_path", help="Path to ini file to create")
+    parser.add_argument("--mysql_host", required=True, help="MySQL host server to use")
+    parser.add_argument(
+        "--mysql_port", default=3306, help="MySQL port to use. Default to 3306"
     )
-    sys.exit(1)
-
-
-def main(argv=sys.argv):
-    if len(argv) != 2:
-        usage(argv)
-    if os.path.exists(argv[1]):
-        print("Ini file already exists")
-        sys.exit(1)
-
+    parser.add_argument(
+        "--mysql_schema",
+        default="formshare",
+        help="MySQL schema to use. Default to 'formshare'",
+    )
+    parser.add_argument(
+        "--mysql_user_name",
+        required=True,
+        help="MySQL user name to use to create the schema",
+    )
+    parser.add_argument(
+        "--mysql_user_password", required=True, help="MySQL user name password"
+    )
+    args = parser.parse_args()
     formshare_path = "."
+
+    main_secret = random_password(13).replace("%", "~")
+    assistant_secret = random_password(13).replace("%", "!")
+    auth_secret = random_password(13).replace("%", "#")
+    aes_key = random_password(28).replace("%", "#")
+    auth_opaque = uuid.uuid4().hex
 
     template_environment = Environment(
         autoescape=False,
@@ -26,15 +56,26 @@ def main(argv=sys.argv):
         trim_blocks=False,
     )
 
-    context = {}
+    context = {
+        "mysql_host": args.mysql_host,
+        "mysql_port": args.mysql_port,
+        "mysql_schema": args.mysql_schema,
+        "mysql_user_name": args.mysql_user_name,
+        "mysql_user_password": args.mysql_user_password,
+        "main_secret": main_secret,
+        "assistant_secret": assistant_secret,
+        "auth_secret": auth_secret,
+        "aes_key": aes_key,
+        "auth_opaque": auth_opaque,
+    }
     rendered_template = template_environment.get_template("formshare.jinja2").render(
         context
     )
 
-    with open(argv[1], "w") as f:
+    with open(args.ini_path, "w") as f:
         f.write(rendered_template)
 
-    print("FormShare INI file created at {}".format(argv[1]))
+    print("FormShare INI file created at {}".format(args.ini_path))
 
 
 if __name__ == "__main__":

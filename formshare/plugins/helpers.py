c02d1df49ff885ef42ce518730b3d20e17a0f2df
Carlos Quiros <cquiros@qlands.com>
2020-05-30 15:01:01 -0600

    Bump version to 2.6.3

--- formshare/plugins/helpers.py
+++ formshare/plugins/helpers.py
@@ -78,7 +78,7 @@ def get_version():
     This returns the version of FormShare
     :return: The version of FormShare
     """
-    return "2.6.2 (20200515)"
+    return "2.6.3 (20200530)"
 
 
 @core_helper

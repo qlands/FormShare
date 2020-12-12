f7da487fdb419e3f008eae5d22fc9a0e904d1bc1
qlands <cquiros@qlands.com>
2020-12-11 18:54:50 -0600

    Fix coverage problem with gevent. More tests

--- formshare/tests/test_00_root.py
+++ formshare/tests/test_00_root.py
@@ -2850,7 +2850,7 @@ class FunctionalTests(unittest.TestCase):
             )
             assert "FS_error" not in res.headers
 
-            # Assistant login succeeds.
+            # Assistant logout succeeds.
             res = self.testapp.post(
                 "/user/{}/project/{}/assistantaccess/logout".format(
                     self.randonLogin, self.project
@@ -3492,6 +3492,200 @@ class FunctionalTests(unittest.TestCase):
             a = color.hex
             b = color.rgb
 
+        def test_one_user_assistant():
+            random_login = str(uuid.uuid4())
+            random_login = random_login[-12:]
+
+            # Test logout
+            self.testapp.get("/logout", status=302)
+
+            # Register succeed
+            res = self.testapp.post(
+                "/join",
+                {
+                    "user_address": "Costa Rica",
+                    "user_email": random_login + "@qlands.com",
+                    "user_password": "123",
+                    "user_id": random_login,
+                    "user_password2": "123",
+                    "user_name": "Testing",
+                    "user_super": "1",
+                    "user_apikey": str(uuid.uuid4()),
+                },
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            # Login succeed
+            res = self.testapp.post(
+                "/login",
+                {"user": "", "email": random_login, "passwd": "123"},
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            project_id = str(uuid.uuid4())
+            # Add a project succeed.
+            res = self.testapp.post(
+                "/user/{}/projects/add".format(random_login),
+                {
+                    "project_id": project_id,
+                    "project_code": "test001",
+                    "project_name": "Test project",
+                    "project_abstract": "",
+                },
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            # Add assistant
+            res = self.testapp.post(
+                "/user/{}/project/{}/assistants/add".format(
+                    random_login, "test001"
+                ),
+                {
+                    "coll_id": "assistant001",
+                    "coll_password": "123",
+                    "coll_password2": "123",
+                    "coll_prjshare": 1,
+                },
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            # Upload a form a succeeds
+            paths = ["resources", "forms", "form08_OK.xlsx"]
+            resource_file = os.path.join(self.path, *paths)
+
+            res = self.testapp.post(
+                "/user/{}/project/{}/forms/add".format(random_login, "test001"),
+                status=302,
+                upload_files=[("xlsx", resource_file)],
+            )
+            assert "FS_error" not in res.headers
+
+            # Add an assistant to a form succeeds
+            res = self.testapp.post(
+                "/user/{}/project/{}/form/{}/assistants/add".format(
+                    random_login, "test001", "Justtest"
+                ),
+                {
+                    "coll_id": "{}|{}".format(project_id, "assistant001"),
+                    "coll_privileges": "1",
+                },
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            res = self.testapp.post(
+                "/user/{}/project/{}/assistantaccess/login".format(
+                    random_login, "test001"
+                ),
+                {"login": "assistant001", "passwd": "123"},
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            # Assistant logout succeeds.
+            res = self.testapp.post(
+                "/user/{}/project/{}/assistantaccess/logout".format(
+                    random_login, "test001"
+                ),
+                {"login": "assistant001", "passwd": "123"},
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            # Add an second assistant to differentt project
+
+            project_id = str(uuid.uuid4())
+            # Add a project 2 succeed.
+            res = self.testapp.post(
+                "/user/{}/projects/add".format(random_login),
+                {
+                    "project_id": project_id,
+                    "project_code": "test002",
+                    "project_name": "Test project",
+                    "project_abstract": "",
+                },
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            # Ass assistant to project 2
+            res = self.testapp.post(
+                "/user/{}/project/{}/assistants/add".format(
+                    random_login, "test002"
+                ),
+                {
+                    "coll_id": "assistant002",
+                    "coll_password": "123",
+                    "coll_password2": "123",
+                    "coll_prjshare": 1,
+                },
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            # Add an assistant of project 2 to a form succeeds
+            res = self.testapp.post(
+                "/user/{}/project/{}/form/{}/assistants/add".format(
+                    random_login, "test001", "Justtest"
+                ),
+                {
+                    "coll_id": "{}|{}".format(project_id, "assistant002"),
+                    "coll_privileges": "1",
+                },
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            res = self.testapp.post(
+                "/user/{}/project/{}/assistantaccess/login".format(
+                    random_login, "test001"
+                ),
+                {"login": "assistant002", "passwd": "123"},
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            # Test getting the forms.
+            self.testapp.get(
+                "/user/{}/project/{}/formList".format(random_login, "test001"),
+                status=200,
+                extra_environ=dict(
+                    FS_for_testing="true", FS_user_for_testing="assistant002"
+                ),
+            )
+
+            # Set the assistant as not shareable
+            res = self.testapp.post(
+                "/user/{}/project/{}/assistant/{}/edit".format(
+                    random_login, "test001", "assistant002"
+                ),
+                {"coll_id": "assistant002"},
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            res = self.testapp.post(
+                "/user/{}/project/{}/assistantaccess/login".format(
+                    random_login, "test001"
+                ),
+                {"login": "assistant002", "passwd": "123"},
+                status=302,
+            )
+            assert "FS_error" not in res.headers
+
+            # Test getting the forms.
+            self.testapp.get(
+                "/user/{}/project/{}/formList".format(random_login, "test001"),
+                status=200,
+                extra_environ=dict(
+                    FS_for_testing="true", FS_user_for_testing="assistant002"
+                ),
+            )
+
         test_root()
         test_login()
         test_dashboard()
@@ -3516,3 +3710,4 @@ class FunctionalTests(unittest.TestCase):
         test_utility_functions()
         test_avatar_generator()
         test_color_hash_hex()
+        test_one_user_assistant()

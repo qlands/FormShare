bb5fd2ab099e5fceff9d9396e6e679aedb52aad9
qlands <cquiros@qlands.com>
2021-01-14 00:32:12 -0600

    Bump stable to 2.7.0

--- formshare/plugins/helpers.py
+++ formshare/plugins/helpers.py
@@ -78,7 +78,7 @@ def get_version():
     This returns the version of FormShare
     :return: The version of FormShare
     """
-    return "2.6.13 (20201129)"
+    return "2.7.0 (20210114)"
 
 
 @core_helper
--- setup.py
+++ setup.py
@@ -159,7 +159,7 @@ tests_require = ["WebTest >= 1.3.1", "pytest", "pytest-cov"]  # py3 compat
 
 setup(
     name="formshare",
-    version="2.6.13",
+    version="2.7.0",
     description="FormShare",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[

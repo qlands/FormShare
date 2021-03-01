a729eb82326c96b7a6df9efa470e48a03b8d6e80
qlands <cquiros@qlands.com>
2021-03-01 17:23:35 -0600

    Handle better error in support files

--- formshare/views/repository.py
+++ formshare/views/repository.py
@@ -1,6 +1,6 @@
 import json
 import logging
-
+import os
 from lxml import etree
 from pyramid.httpexceptions import HTTPNotFound, HTTPFound
 
@@ -217,6 +217,24 @@ class GenerateRepository(PrivateView):
                                         message,
                                     )
                                     stage = -1
+
+                            if result_code == 14:
+                                txt_message = 'The following files have invalid characters like extra ". ' \
+                                              'Only _ is allowed : \n'
+                                root = etree.fromstring(message)
+                                files_with_problems = root.findall(".//file")
+                                if files_with_problems:
+                                    for a_file in files_with_problems:
+                                        txt_message = txt_message + "\t" + os.path.basename(a_file.get("name","")) + "\n"
+                                self.append_to_errors(txt_message)
+                            if result_code == 15:
+                                txt_message = "The following files have an invalid structure: \n"
+                                root = etree.fromstring(message)
+                                files_with_problems = root.findall(".//file")
+                                if files_with_problems:
+                                    for a_file in files_with_problems:
+                                        txt_message = txt_message + "\t" + a_file.get("name","") + "\n"
+                                self.append_to_errors(txt_message)
 
                             if result_code == 9:
                                 # Duplicated options

8b04c96bbbcdca172017c59fe3c716a652f4e541
qlands <cquiros@qlands.com>
2021-03-21 09:23:52 -0600

    Add table and field index to dict

---
+++ alembic/versions/82aca4b87729_add_table_index.py
@@ -0,0 +1,35 @@
+"""Add table index
+
+Revision ID: 82aca4b87729
+Revises: 2a754491f4bc
+Create Date: 2021-03-21 08:49:48.758600
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.orm.session import Session
+
+
+# revision identifiers, used by Alembic.
+revision = "82aca4b87729"
+down_revision = "2a754491f4bc"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "dicttable",
+        sa.Column("table_index", sa.BigInteger, autoincrement=True, nullable=True),
+    )
+    op.create_index(
+        op.f("ix_dicttable_table_index"), "dicttable", ["table_index"], unique=True
+    )
+    conn = op.get_bind()
+    conn.execute(
+        "ALTER TABLE dicttable MODIFY COLUMN table_index BIGINT AUTO_INCREMENT"
+    )
+
+
+def downgrade():
+    op.drop_column("dicttable", "table_index")

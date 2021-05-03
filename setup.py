0aaa2981f09edd33542280d872530a1d4f8a7829
qlands <cquiros@qlands.com>
2021-05-03 07:06:37 -0600

    Bump stable to 2.8.2

--- setup.py
+++ setup.py
@@ -156,7 +156,7 @@ tests_require = ["WebTest >= 1.3.1", "pytest", "pytest-cov"]  # py3 compat
 
 setup(
     name="formshare",
-    version="2.8.1",
+    version="2.8.2",
     description="FormShare",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[

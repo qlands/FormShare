da9faf64c8caec62b3dffd0b45f275fbf42c841e
qlands <cquiros@qlands.com>
2021-07-21 15:25:33 -0400

    Upgrade Celery. Use celery pool

--- start_local_celery.sh
+++ start_local_celery.sh
@@ -1,1 +1,1 @@
-celery -A formshare.config.celery_app worker  --loglevel=info -Q FormShare  --broker=redis://localhost:6379/0 --result-backend=redis://localhost:6379/0
+celery -A formshare.config.celery_app worker  --loglevel=info -Q FormShare

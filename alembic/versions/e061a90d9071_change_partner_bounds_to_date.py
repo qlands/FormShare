fe16f1c4c4c1e7bc26c3b3f1df1390fca3055f53
QLands <cquiros@qlands.com>
2022-04-04 10:15:29 -0400

    Change partner bounds to date

---
+++ alembic/versions/e061a90d9071_change_partner_bounds_to_date.py
@@ -0,0 +1,50 @@
+"""Change partner bounds to date
+
+Revision ID: e061a90d9071
+Revises: 029c1e779679
+Create Date: 2022-04-03 21:19:27.788492
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision = "e061a90d9071"
+down_revision = "029c1e779679"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column(
+        "partnerproject",
+        "access_from",
+        existing_type=mysql.DATETIME(),
+        type_=sa.Date(),
+        existing_nullable=True,
+    )
+    op.alter_column(
+        "partnerproject",
+        "access_to",
+        existing_type=mysql.DATETIME(),
+        type_=sa.Date(),
+        existing_nullable=True,
+    )
+
+
+def downgrade():
+    op.alter_column(
+        "partnerproject",
+        "access_to",
+        existing_type=sa.Date(),
+        type_=mysql.DATETIME(),
+        existing_nullable=True,
+    )
+    op.alter_column(
+        "partnerproject",
+        "access_from",
+        existing_type=sa.Date(),
+        type_=mysql.DATETIME(),
+        existing_nullable=True,
+    )

472af419e82aadef9a7d25e70a8ff5d1ff305591
Carlos Quiros <cquiros@qlands.com>
2022-08-03 12:00:34 -0400

    Add local celery for testing

--- start_local_celery.sh
+++ start_local_celery.sh
@@ -1,3 +1,3 @@
 export FORMSHARE_RUN_FROM_CELERY=true
-celery -A formshare.config.celery_app worker --loglevel=info -Q FormShare -f ./celery.log
+celery -A formshare.config.celery_app worker --loglevel=info -Q FormShare
 export FORMSHARE_RUN_FROM_CELERY=false

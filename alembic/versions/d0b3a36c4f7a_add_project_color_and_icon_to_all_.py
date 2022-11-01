f6d2d3c12b221f32a35266a89729ccd25d50664a
qlands <cquiros@qlands.com>
2022-11-01 07:40:04 -0400

    Fix emoji migration

--- alembic/versions/d0b3a36c4f7a_add_project_color_and_icon_to_all_.py
+++ alembic/versions/d0b3a36c4f7a_add_project_color_and_icon_to_all_.py
@@ -8,7 +8,7 @@ Create Date: 2021-07-29 15:27:36.822801
 import random
 
 from alembic import op
-from emoji.unicode_codes import EMOJI_UNICODE_ENGLISH
+from emoji.unicode_codes import get_emoji_unicode_dict
 from sqlalchemy.orm.session import Session
 
 from formshare.models.formshare import Project
@@ -26,10 +26,11 @@ def upgrade():
     projects = session.query(
         Project.project_id, Project.project_name, Project.project_code
     ).all()
+    emoji_unicode_english = get_emoji_unicode_dict("en")
     for a_project in projects:
         emoji_array = []
-        for a_key in EMOJI_UNICODE_ENGLISH.keys():
-            if len(EMOJI_UNICODE_ENGLISH[a_key]) == 1:
+        for a_key in emoji_unicode_english.keys():
+            if len(emoji_unicode_english[a_key]) == 1:
                 emoji_array.append(a_key)
         emojis = len(emoji_array)
         emoji_selected = random.randint(0, emojis - 1)
@@ -37,7 +38,7 @@ def upgrade():
 
         project_data = {
             "project_hexcolor": ColorHash(a_project.project_name).hex,
-            "project_icon": EMOJI_UNICODE_ENGLISH[emoji_selected],
+            "project_icon": emoji_unicode_english[emoji_selected],
         }
         session.query(Project).filter(
             Project.project_id == a_project.project_id

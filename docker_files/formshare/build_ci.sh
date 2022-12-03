f88b5d4ae8af689f3c99e4b47a365686dec1b0dc
qlands <cquiros@qlands.com>
2022-12-03 08:12:20 -0500

    Update cI

--- docker_files/formshare/build_ci.sh
+++ docker_files/formshare/build_ci.sh
@@ -1,1 +1,1 @@
-sudo docker build --no-cache --rm -t qlands/odktools_circleci:20221031 -f ./circleci.Dockerfile .
+sudo docker build --no-cache --rm -t qlands/odktools_circleci:20221202 -f ./circleci.Dockerfile .

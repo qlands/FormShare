3dddb91993e8ac85a58754e60040d9f3fe6161c7
qlands <cquiros@qlands.com>
2022-12-20 13:07:37 -0500

    Remove any so code is compatible with python 3.8

--- formshare/i18n/extract.py
+++ formshare/i18n/extract.py
@@ -1,16 +1,18 @@
-from typing import Any
 from jinja2.ext import babel_extract
-from formshare.config.jinja_extensions import get_extensions
 
 
-def extract_formshare(fileobj: Any, *args: Any, **kw: Any) -> Any:
+jinja_extensions = """
+                    jinja2.ext.do,jinja2.ext.i18n,                    
+                    formshare.config.jinja_extensions:JSResourceExtension,
+                    formshare.config.jinja_extensions:CSSResourceExtension,
+                    formshare.config.jinja_extensions:ExtendThis,
+                   """
+
+
+def extract_formshare(fileobj, *args, **kw):
     """This code is based on CKAN
     :Copyright (c) 2006-2018 Open Knowledge Foundation and contributors
     :license: AGPL V3, see LICENSE for more details."""
-    extensions = [
-        ":".join([ext.__module__, ext.__name__]) if isinstance(ext, type) else ext
-        for ext in get_extensions()
-    ]
     if "options" not in kw:
         kw["options"] = {}
     if "trimmed" not in kw["options"]:
@@ -18,6 +20,6 @@ def extract_formshare(fileobj: Any, *args: Any, **kw: Any) -> Any:
     if "silent" not in kw["options"]:
         kw["options"]["silent"] = "False"
     if "extensions" not in kw["options"]:
-        kw["options"]["extensions"] = ",".join(extensions)
+        kw["options"]["extensions"] = jinja_extensions
 
     return babel_extract(fileobj, *args, **kw)

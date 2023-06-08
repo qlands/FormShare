a9882595be65fe3fdd6a045f4f3bd69179bc7247
qlands <cquiros@qlands.com>
2023-06-08 15:43:21 -0400

    New tools to extract files

---
+++ utilities/extract_file.py
@@ -0,0 +1,7 @@
+from formshare.processes.storage import get_stream
+
+# project = ""
+#
+# bucket_id = project + form
+# bucket_id = md5(bucket_id.encode("utf-8")).hexdigest()
+#     stream = get_stream(request, bucket_id, file_name)

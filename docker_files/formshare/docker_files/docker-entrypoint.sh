e94b3aa8c294ff9337163310ca14eafaff0bcfa6
qlands <cquiros@qlands.com>
2023-09-04 13:58:12 -0400

    Fix Redis under new Ubuntu image

--- docker_files/formshare/docker_files/docker-entrypoint.sh
+++ docker_files/formshare/docker_files/docker-entrypoint.sh
@@ -3,6 +3,12 @@
 rm /var/run/mongod.pid
 mongod --fork --pidfilepath /var/run/mongod.pid --logpath /var/log/mongodb/mongod.log --config /etc/mongod.conf
 rm /var/run/redis/redis-server.pid
+chown -R redis /etc/redis
+chgrp -R redis /etc/redis
+chown -R redis /var/log/redis
+chgrp -R redis /var/log/redis
+chown -R redis /var/lib/redis
+chgrp -R redis /var/lib/redis
 /etc/init.d/redis-server start
 
 set -e

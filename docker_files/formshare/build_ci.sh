d72b8ce9674163f418abc9e89244138a1b008ead
qlands <cquiros@qlands.com>
2024-04-19 09:44:23 -0400

    Update ODK tools to 2.12

--- docker_files/formshare/build_ci.sh
+++ docker_files/formshare/build_ci.sh
@@ -1,1 +1,1 @@
-sudo docker build  --rm -t qlands/odktools_circleci:20240327 -f ./circleci.Dockerfile .
+sudo docker build --no-cache --rm -t qlands/odktools_circleci:20240418 -f ./circleci.Dockerfile .

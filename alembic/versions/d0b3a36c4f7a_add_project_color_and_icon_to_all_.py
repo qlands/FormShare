5aea41322f68acabe1ee45ff2e085fb0521f1ce0
qlands <cquiros@qlands.com>
2026-02-14 16:35:23 -0500

    Remove broken code

--- alembic/versions/d0b3a36c4f7a_add_project_color_and_icon_to_all_.py
+++ alembic/versions/d0b3a36c4f7a_add_project_color_and_icon_to_all_.py
@@ -9,7 +9,6 @@ Create Date: 2021-07-29 15:27:36.822801
 import random
 
 from alembic import op
-from emoji.unicode_codes import get_emoji_unicode_dict
 from formshare.models.formshare import Project
 from formshare.processes.color_hash import ColorHash
 from sqlalchemy.orm.session import Session
@@ -26,19 +25,10 @@ def upgrade():
     projects = session.query(
         Project.project_id, Project.project_name, Project.project_code
     ).all()
-    emoji_unicode_english = get_emoji_unicode_dict("en")
     for a_project in projects:
-        emoji_array = []
-        for a_key in emoji_unicode_english.keys():
-            if len(emoji_unicode_english[a_key]) == 1:
-                emoji_array.append(a_key)
-        emojis = len(emoji_array)
-        emoji_selected = random.randint(0, emojis - 1)
-        emoji_selected = emoji_array[emoji_selected]
-
         project_data = {
             "project_hexcolor": ColorHash(a_project.project_name).hex,
-            "project_icon": emoji_unicode_english[emoji_selected],
+            "project_icon": "⭐",
         }
         session.query(Project).filter(
             Project.project_id == a_project.project_id

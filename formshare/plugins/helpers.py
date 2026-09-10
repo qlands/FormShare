38eb9c29e92b5d7d0a717e730020eaf053b526c2
qlands <cquiros@qlands.com>
2026-09-10 14:33:05 -0400

    Bump minimal version

--- formshare/plugins/helpers.py
+++ formshare/plugins/helpers.py
@@ -111,7 +111,7 @@ def get_version():
     This returns the version of FormShare
     :return: The version of FormShare
     """
-    return "3.0.0 (20260409)"
+    return "3.0.1 (20260910)"
 
 
 @core_helper
